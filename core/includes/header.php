45cb918a89d0f88326cdd37a6b299d7eeb949a23
Алексей Семенов <alexey.semenov@starcode.ru>
2016-09-05 11:56:24 +0300

    #0002api Забытый файл

---
+++ core/includes/header.php
@@ -0,0 +1,11 @@
+<?
+//Временно, будем выносить в autoload
+require_once ('config/db.php');
+require_once ('core/interfaces/Data.php');
+require_once ('core/classes/Database.php');
+require_once ('core/classes/DataPreparer.php');
+require_once ('core/classes/Writer.php');
+require_once ('core/classes/Reader.php');
+require_once ('core/classes/Item.php');
+
+?>

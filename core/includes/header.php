76a42310440257f52e238fcd1120aaebb8a64f8c
Алексей Семенов <alexey.semenov@starcode.ru>
2016-10-25 11:40:10 +0300

    Вынес автолоадер в отдельный файл-2

--- core/includes/header.php
+++ core/includes/header.php
@@ -1,7 +1,5 @@
 <?
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/config/db.php');
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/local/functions/functions.php');
-
 require_once dirname(__DIR__) . '/autoload.php';
-echo __DIR__;
 ?>

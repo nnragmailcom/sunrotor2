7d87c0bf0b3720129b1792e02ef174e21c5d6813
Алексей Семенов <alexey.semenov@starcode.ru>
2016-10-26 15:17:00 +0300

    Небольшие правки вывода

--- templates/detail.php
+++ templates/detail.php
@@ -1,7 +1,18 @@
 <div class="items-container">
-	<pre>
-	    <?var_dump($arData);?>
-	</pre>
+	<div class="item-detail">
+		<div class="name">
+			<h1><?=$arData['0']["NAME"]?></h1>
+		</div>
+		<div class="img">
+			<img src="<?=$arData['0']["DETAIL_PIC_LINK"]?>" alt="" />
+		</div>
+		<div class="datetime">
+			<?=$arData['0']["TIME"]?>
+		</div>
+		<div class="detail-text">
+			<?=$arData['0']["DETAIL_TEXT"]?>	
+		</div>
+	</div>
 </div>
-<a href="/">Index</a> <br>
-<a href="/inside/add/">Inside</a>
+<a href="/">Index</a>
+<a href="/inside">Inside</a>
